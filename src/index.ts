5aeeb4b0cbc88fb57cfb5b105d1b873498531cd1
EmanoelSaraiva <emanoelsaraivas@gmail.com>
2023-10-20 11:48:44 -0300

    route default

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,13 @@ app.use('/like', likeRoutes());
 app.use('/retweet', retweetRoutes());
 app.use('/follow', followRouter());
 app.use('/auth', authRoutes());
+app.use('/', (req, res) => {
+  res.status(200).send({
+    ok: true,
+    code: 200,
+    message: 'Api Growtwitter',
+  });
+});
 
 app.listen(3333, () => {
   console.log('API rodando na porta 3333');
